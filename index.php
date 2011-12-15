4d3538026b7680cb9f9558e1d9cfc9ad92297c2e
Marcus Kielly <marcus@marcusk.co.uk>
2011-12-15 23:57:42 +0000

    little bit of fun with rotation animation - exposed exponential scaling issues

--- index.php
+++ index.php
@@ -4,23 +4,30 @@ include 'partials/header.php';
 ?>
 <canvas id="ezl" width="900" height="900"></canvas>
 <script>
-(function(){
-    var sqr = form.rec(50,50),
+
+    (function(){
+        var sqr = form.rec(50,50),
         crc = form.crc(100),
-        ctx = document.getElementById('ezl').getContext('2d');
+        ctx = document.getElementById('ezl').getContext('2d'),
+        n = 0;
+        ctx.fillStyle = 'rgba(0,0,0,0)';
+        sqr.scale(2,1);
+        crc.scale(1,2);
 
+        function animated() {
+            if(n < 360) {
+                ctx.clearRect(0, 0, 960, 400);
+                crc.rotate(1);
+                sqr.rotate(-2);
+                sqr.draw(ctx,[200,200]);
+                crc.draw(ctx,[200,200]);
+                setTimeout(animated,1);
+            }
+            n += 1;
+        }
 
-    ctx.lineWidth = 1;
-    ctx.beginPath();
-    ctx.strokeStyle = 'rgba(0,0,0,100)';
-    ctx.fillStyle = 'rgba(0,0,0,0)';
-    
-    sqr.scale(2,1);
-    sqr.draw(ctx,[200,200]);
-    crc.scale(2,1);
-    crc.rotate(45);
-    crc.draw(ctx,[200,200]);
-}());
+        animated();
+    }());
 
 </script>
 
